c71ff66f9208537831fc9364be7d19f751d5b48c
tatarynrm <tatarynrm@gmail.com>
2024-10-10 10:01:20 +0300

    ewqewqewq

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ app.get("/manage", async (req, res) => {
 
 
 app.get('/instagram/webhook',async (req,res) =>{
-  const VERIFY_TOKEN = 'your-verification-token';
+  const VERIFY_TOKEN = process.env.INSTAGRAM_VERIFY_TOKEN;
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
